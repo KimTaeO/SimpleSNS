cc02168d951665c42c22ca11c5b45d55588ac414
KimTaeO <taeo0328@gmail.com>
2023-08-19 16:35:31 +0900

    :sparkles: :: 로그인된 유저 정보 조회 util 코드 작성

---
+++ src/main/kotlin/com/toyproject/simplesns/domain/user/util/UserUtil.kt
@@ -0,0 +1,24 @@
+package com.toyproject.simplesns.domain.user.util
+
+import com.toyproject.simplesns.domain.user.entity.User
+import com.toyproject.simplesns.domain.user.repository.UserRepository
+import com.toyproject.simplesns.global.exception.exceptions.UserNotFoundException
+import org.springframework.security.core.context.SecurityContextHolder
+import org.springframework.stereotype.Component
+
+@Component
+class UserUtil(
+    private val userRepository: UserRepository
+) {
+    fun fetchCurrentUser(): User {
+        val principal = SecurityContextHolder.getContext().authentication.principal
+        val phoneNumber = principal.toString()
+        return fetchUserByPhoneNumber(phoneNumber)
+    }
+
+    private fun fetchUserByPhoneNumber(phoneNum: String): User =
+
+        
+        userRepository.findByPhoneNumber(phoneNum) ?: throw UserNotFoundException()
+
+}
