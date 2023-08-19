1e1059f8eb7a07ef35448ca562c9f585a6c97c46
KimTaeO <taeo0328@gmail.com>
2023-08-19 17:54:34 +0900

    :sparkles: :: 게시물에 유저와 매핑 추가

--- src/main/kotlin/com/toyproject/simplesns/domain/post/entity/Post.kt
+++ src/main/kotlin/com/toyproject/simplesns/domain/post/entity/Post.kt
@@ -1,14 +1,19 @@
 package com.toyproject.simplesns.domain.post.entity
 
+import com.toyproject.simplesns.domain.user.entity.User
 import javax.persistence.*
 
 @Entity
-class Post (
+class Post(
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     val id: Long = 0L,
 
     val content: String,
+
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    val user:User,
 
     @ElementCollection(fetch = FetchType.EAGER)
     @CollectionTable(name = "img", joinColumns = [JoinColumn(name = "id")])
