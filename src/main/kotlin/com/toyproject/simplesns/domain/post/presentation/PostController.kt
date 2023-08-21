f26b5b938144311ad38223933e249be056235e40
KimTaeO <taeo0328@gmail.com>
2023-08-21 14:19:14 +0900

    :sparkles: :: 공개범위 변경 컨트롤러 작성

--- src/main/kotlin/com/toyproject/simplesns/domain/post/presentation/PostController.kt
+++ src/main/kotlin/com/toyproject/simplesns/domain/post/presentation/PostController.kt
@@ -1,9 +1,14 @@
 package com.toyproject.simplesns.domain.post.presentation
 
 import com.toyproject.simplesns.domain.post.presentation.dto.request.CreatePostRequest
+import com.toyproject.simplesns.domain.post.service.ChangeVisibilityFalseService
+import com.toyproject.simplesns.domain.post.service.ChangeVisibilityTrueService
 import com.toyproject.simplesns.domain.post.service.CreatePostService
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.PatchMapping
+import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
@@ -11,10 +16,25 @@ import org.springframework.web.bind.annotation.RestController
 @RestController
 @RequestMapping("/post")
 class PostController(
-    private val createPostService: CreatePostService
+    private val createPostService: CreatePostService,
+    private val changeVisibilityTrueService: ChangeVisibilityTrueService,
+    private val changeVisibilityFalseService: ChangeVisibilityFalseService
 ) {
+    @PostMapping
     fun createPost(@RequestBody createPostRequest: CreatePostRequest): ResponseEntity<Void> {
         createPostService.execute(createPostRequest)
         return ResponseEntity(HttpStatus.CREATED)
+    }
+
+    @PatchMapping("/{post_id}")
+    fun changeVisibilityTrue(@PathVariable("post_id") postId: Long): ResponseEntity<Void> {
+        changeVisibilityTrueService.execute(postId)
+        return ResponseEntity(HttpStatus.OK)
+    }
+
+    @PatchMapping("/{post_id}")
+    fun changeVisibilityFalse(@PathVariable("post_id") postId: Long): ResponseEntity<Void> {
+        changeVisibilityFalseService.execute(postId)
+        return ResponseEntity(HttpStatus.OK)
     }
 }
