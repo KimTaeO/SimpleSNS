8c032e88ee4e3fa03fe5cf90252ad8902ba81278
KimTaeO <taeo0328@gmail.com>
2023-08-21 15:13:59 +0900

    :sparkles: :: 게시물 삭제 컨트롤러 작성

--- src/main/kotlin/com/toyproject/simplesns/domain/post/presentation/PostController.kt
+++ src/main/kotlin/com/toyproject/simplesns/domain/post/presentation/PostController.kt
@@ -4,8 +4,10 @@ import com.toyproject.simplesns.domain.post.presentation.dto.request.CreatePostR
 import com.toyproject.simplesns.domain.post.service.ChangeVisibilityFalseService
 import com.toyproject.simplesns.domain.post.service.ChangeVisibilityTrueService
 import com.toyproject.simplesns.domain.post.service.CreatePostService
+import com.toyproject.simplesns.domain.post.service.DeletePostService
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.DeleteMapping
 import org.springframework.web.bind.annotation.PatchMapping
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.PostMapping
@@ -18,7 +20,8 @@ import org.springframework.web.bind.annotation.RestController
 class PostController(
     private val createPostService: CreatePostService,
     private val changeVisibilityTrueService: ChangeVisibilityTrueService,
-    private val changeVisibilityFalseService: ChangeVisibilityFalseService
+    private val changeVisibilityFalseService: ChangeVisibilityFalseService,
+    private val deletePostService: DeletePostService
 ) {
     @PostMapping
     fun createPost(@RequestBody createPostRequest: CreatePostRequest): ResponseEntity<Void> {
@@ -36,5 +39,11 @@ class PostController(
     fun changeVisibilityFalse(@PathVariable("post_id") postId: Long): ResponseEntity<Void> {
         changeVisibilityFalseService.execute(postId)
         return ResponseEntity(HttpStatus.OK)
+    }
+
+    @DeleteMapping("/{post_id}")
+    fun deletePost(@PathVariable("post_id") postId: Long): ResponseEntity<Void> {
+        deletePostService.execute(postId)
+        return ResponseEntity(HttpStatus.NO_CONTENT)
     }
 }
